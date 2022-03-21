7234576d1f4174c1ab84f2446be39137c6c3fc52
oznerol <lorenzo.ghidoni92@gmail.com>
2022-03-21 12:06:48 +0000

    fix grids

--- public/src/Components/ProductsList/ProductsList.js
+++ public/src/Components/ProductsList/ProductsList.js
@@ -2,32 +2,66 @@ import React, {useEffect} from 'react'
 import axios from 'axios'
 import ProductCard from './ProductCard'
 import { Grid } from '@mui/material'
+import { Typography } from '@mui/material'
 
 export default function ProductsList(props) {
+    const [editable, setEditable] = React.useState(false)
+    const [list, setList] = React.useState([])
+
+    const handleEditable = () => {
+
+    }
+
+    const createList = () => {
+        const result = props.productsList.reduce((acc, d) => {
+            const found = acc.find(a => a.category === d.category)
+            //const value = { category: d.category, val: d.value }
+            const value = d // the element in data property
+            if (!found) {
+                //acc.push(...value)
+                acc.push({category:d.category, products: [value]}) // not found, so need to add products property
+            }
+            else {
+                //acc.push({ category: d.category, products: [{ value: d.value }, { name: d.name }] })
+                found.products.push(value) // if found, that means products property exists, so just push new element to found.data.
+            }
+            return acc
+        }, [])
+        
+        setList(result)
+    }
+
+    useEffect(() => {
+        createList()
+    }, [])
+
     return (
         <div>
-            Products list<br/><br/>
-            {props.categories
-            ? props.categories.map(category => (
-                <div key={category}>
-                    {category}
-                    <Grid container>
-                        {props.productsList
-                        ? props.productsList.map((product) => (
-                            product.category === category
-                            ?   <div key={product._id}>
-                                    <ProductCard
-                                        product={product}
-                                        addToCart={props.addToCart}
-                                    /><br/>
-                                </div>
-                            : null))
-                        : null
-                        }
+            {props.user.admin
+            ? 'admin editable button'
+            : 'user'
+            }
+            {editable
+            ? 'editable for admin'
+            : 'user'
+            }
+            <Grid container direction='column' spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            {list.map((item, index) => (
+                <Grid item container xs={2} sm={4} md={4} key={index}>
+                    {index} {item.category}
+                    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                        {item.products.map((product, i) => (
+                        <Grid item xs={2}>
+                            <ProductCard
+                                product={product}
+                                addToCart={props.addToCart}
+                            />
+                        </Grid>
+                        ))}
                     </Grid>
-                </div>
-            ))
-            : null}
+                </Grid>
+            ))}
+            </Grid>
         </div>
     )
 }
