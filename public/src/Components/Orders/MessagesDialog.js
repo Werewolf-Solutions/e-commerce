d5845fe3c961aa6fd4b0dd5763e0bcf11c64cb7b
oznerol <lorenzo.ghidoni92@gmail.com>
2022-03-31 17:05:03 +0100

    add socket.io

--- public/src/Components/Orders/MessagesDialog.js
+++ public/src/Components/Orders/MessagesDialog.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Dialog } from '@mui/material'
 import { Typography } from '@mui/material'
 import { TextField } from '@mui/material'
 import { Button } from '@mui/material'
+
+import { io } from 'socket.io-client'
+
 import axios from 'axios'
 
+const socket = io('http://localhost:5000', {
+    withCredentials: true,
+    extraHeaders: {
+        "my-custom-header": "abcd"
+    }
+})
+
+
 export default function MessagesDialog(props) {
+    const [chat, setChat] = React.useState([])
     const [message, setMessage] = React.useState()
+
+    useEffect(() => {
+        socket.on('message', ({message}) => {
+            setChat(message)
+        })
+    })
+
     const handleChange = (e) => {
         setMessage(e.target.value)
     }
@@ -16,6 +35,7 @@ export default function MessagesDialog(props) {
         console.log(props.order)
         // TODO: call endpoint
         let res = await axios.post('/users/send-msg', {message, order_id: props.order._id})
+        socket.emit('message', {message})
         console.log(res.data)
         props.updateUser()
         setMessage('')
@@ -32,6 +52,7 @@ export default function MessagesDialog(props) {
                 ))
                 : 'No messages'
                 }
+                <Typography>{chat}</Typography>
                 <TextField
                     id='message'
                     value={message}
