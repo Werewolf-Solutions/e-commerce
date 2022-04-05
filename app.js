7cf1aa5eb5f231c6572ff4878509e15b5a747dfa
oznerol <lorenzo.ghidoni92@gmail.com>
2022-04-05 12:35:23 +0100

    some fixes

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500)
-  res.json('error')
+  res.json('error', err, res.locals.error)
 })
 
 module.exports = app
