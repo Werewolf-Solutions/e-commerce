cfb693ab66aba229c5018c709dd21789969c7c94
oznerol <lorenzo.ghidoni92@gmail.com>
2022-04-05 12:01:28 +0100

    some fixes

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+// app.use('/users', usersRouter)
+app.use('/users', './routes/users')
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
