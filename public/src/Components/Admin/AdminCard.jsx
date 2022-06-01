55143a19cd847dcea648389f84747fc32c84eb77
JonathonW <79091107+JDWilks@users.noreply.github.com>
2022-06-01 10:44:48 +0400

    edit modal and log in and sign up styled

--- public/src/Components/Admin/AdminCard.jsx
+++ public/src/Components/Admin/AdminCard.jsx
@@ -38,7 +38,7 @@ export default function AdminCard(props) {
           <a href="#*" class="btn btn-info ms-3 btn-sm">
             Edit Section header
           </a>
-          <a
+          <button
             class="btn btn-sm ms-2 btn-warning"
             onClick={() => {
               console.log("createProductDialog");
@@ -46,7 +46,7 @@ export default function AdminCard(props) {
             }}
           >
             create new product for this section
-          </a>
+          </button>
         </div>
         {/* side orders cards */}
         <div className="col-md-6 col-lg-4">
--- public/src/Components/SignUp/SignUpDialog.jsx
+++ public/src/Components/SignUp/SignUpDialog.jsx
@@ -1,27 +1,33 @@
-import { Dialog } from '@mui/material'
-import React from 'react'
-import SignUpForm from './SignUpForm'
+import { Dialog } from "@mui/material";
+import React from "react";
+import SignUpForm from "./SignUpForm";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 export default function SignUpDialog(props) {
-    return (
-        <div>
-            <Dialog
-                open={props.open}
-                onClose={props.onClose}
-            >
-                <SignUpForm
-                    signUp={props.signUp}
-                    handleChange={props.handleChange}
-                    handleSignInDialog={props.handleSignInDialog}
-                    handleSignUpDialog={props.handleSignUpDialog}
-                    update={props.update}
-                    onClose={props.onClose}
-                />
-                {/* <button onClick={() => {
+  return (
+    <div>
+      <ThemeProvider theme={darkTheme}>
+        <Dialog open={props.open} onClose={props.onClose}>
+          <SignUpForm
+            signUp={props.signUp}
+            handleChange={props.handleChange}
+            handleSignInDialog={props.handleSignInDialog}
+            handleSignUpDialog={props.handleSignUpDialog}
+            update={props.update}
+            onClose={props.onClose}
+          />
+          {/* <button onClick={() => {
                         props.handleSignInDialog()
                         props.handleSignUpDialog()
                 }}>open sign in</button> */}
-            </Dialog>
-        </div>
-    )
+        </Dialog>
+      </ThemeProvider>
+    </div>
+  );
 }
