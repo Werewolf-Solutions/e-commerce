b0536529daef32c041e4ac64332d5661d4bfdb0d
oznerol <lorenzo.ghidoni92@gmail.com>
2022-09-17 15:28:23 +0100

    socket changes

--- public/src/service/socket.js
+++ public/src/service/socket.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { io } from "socket.io-client";
 
-let SOCKET_URL = "https://werewolf.solutions/e-commerce-demo";
 // let SOCKET_URL = "http://localhost:5001/";
 
-export const socket = io.connect(SOCKET_URL);
+export const socket = io("https://werewolf.solutions", {
+  path: "/e-commerce-demo/",
+});
+
+// export const socket = io.connect(SOCKET_URL);
 // export const socket = io();
 export const SocketContext = React.createContext();
