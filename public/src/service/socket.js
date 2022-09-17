8bb221d41f03c2116a9c1c0531e86ca44b464b14
oznerol <lorenzo.ghidoni92@gmail.com>
2022-09-17 13:02:25 +0100

    prod

--- public/src/service/socket.js
+++ public/src/service/socket.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { io } from 'socket.io-client'
+import React from "react";
+import { io } from "socket.io-client";
 
-let SOCKET_URL = ''
+let SOCKET_URL = "http://localhost:5001/";
 
-export const socket = io()// .connect(SOCKET_URL)
-export const SocketContext = React.createContext()
+export const socket = io.connect(SOCKET_URL); // io();
+export const SocketContext = React.createContext();
