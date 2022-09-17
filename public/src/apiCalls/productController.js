0075b787dcb931ad918a9a394ce7c9b0863cc81a
oznerol <lorenzo.ghidoni92@gmail.com>
2022-09-17 15:17:14 +0100

    socket changes and fix endpoints

--- public/src/apiCalls/productController.js
+++ public/src/apiCalls/productController.js
@@ -1,7 +1,7 @@
-const axios = require('axios')
+const axios = require("axios");
 
 /**
- * 
+ *
  * @desc    get all products
  * @route   GET /users/products
  * @access  Public
@@ -9,12 +9,12 @@ const axios = require('axios')
  * @returns products
  */
 export async function getProducts() {
-    let res = await axios.get('users/products')
-    return res.data.products
+  let res = await axios.get("users/products");
+  return res.data.products;
 }
 
 /**
- * 
+ *
  * @desc    create product
  * @route   POST /users/create-product
  * @access  Private
@@ -22,13 +22,13 @@ export async function getProducts() {
  * @returns product, msg
  */
 export async function createProduct(product) {
-    let res = await axios.post('users/create-product', {product})
-    // console.log(res.data)
-    return res.data.product
+  let res = await axios.post("users/create-product", { product });
+  // console.log(res.data)
+  return res.data.product;
 }
 
 /**
- * 
+ *
  * @desc    update product
  * @route   POST /users/update-product
  * @access  Private
@@ -36,12 +36,12 @@ export async function createProduct(product) {
  * @returns product, msg
  */
 export async function updateProduct(product) {
-    let res = await axios.post('users/update-product', {product})
-    // console.log(res.data)
+  let res = await axios.post("users/update-product", { product });
+  // console.log(res.data)
 }
 
 /**
- * 
+ *
  * @desc    delete product
  * @route   POST /users/delete-product
  * @access  Private
@@ -49,12 +49,12 @@ export async function updateProduct(product) {
  * @returns product, msg
  */
 export async function deleteProduct(product) {
-    let res = await axios.post('users/delete-product', {product})
-    // console.log(res.data)
+  let res = await axios.post("users/delete-product", { product });
+  // console.log(res.data)
 }
 
 /**
- * 
+ *
  * @desc    upload img
  * @route   POST /users/upload-img
  * @access  Private
@@ -62,38 +62,40 @@ export async function deleteProduct(product) {
  * @returns img path, mimetype, filename, msg
  */
 export async function uploadImg(file, product) {
-    // Create an object of formData
-    const formData = new FormData()
+  // Create an object of formData
+  const formData = new FormData();
 
-    // Details of the uploaded file
-    // console.log(file)
+  // Details of the uploaded file
+  // console.log(file)
 
-    // Update the formData object
-    formData.append("file", file, file.name)
+  // Update the formData object
+  formData.append("file", file, file.name);
 
-    // Request made to the backend api
-    // Send formData object
-    // let res = await axios.post("/users/upload-img",
-    //     {product: props.product, file, formData})
-    let res = await axios.post("/users/upload-img", formData)
-    // console.log(res.data)
-    let {img} = res.data
-    product.img = img
-    // console.log(product)
-    let response = await axios.post('/users/update-product', {product})
-    // console.log(response.data)
+  // Request made to the backend api
+  // Send formData object
+  // let res = await axios.post("/users/upload-img",
+  //     {product: props.product, file, formData})
+  let res = await axios.post("users/upload-img", formData);
+  // console.log(res.data)
+  let { img } = res.data;
+  product.img = img;
+  // console.log(product)
+  let response = await axios.post("users/update-product", { product });
+  // console.log(response.data)
 }
 
-
 /**
- * 
+ *
  * @desc    update category
  * @route   POST /users/update-category
  * @access  Private
  * @params  category
  * @returns products, msg
  */
- export async function updateCategory(category, new_category) {
-    let res = await axios.post('users/update-category', {category, new_category})
-    // console.log(res.data)
+export async function updateCategory(category, new_category) {
+  let res = await axios.post("users/update-category", {
+    category,
+    new_category,
+  });
+  // console.log(res.data)
 }
